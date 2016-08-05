3e0838827b458ea80913d8e8c06d7efa4524022e
Dan Nixon <dan@dan-nixon.com>
2016-08-05 21:39:50 +0100

    clang-format
    fixin' jon's code

--- library/examples/AppManager/NPPattern.h
+++ library/examples/AppManager/NPPattern.h
@@ -1,38 +1,53 @@
 #ifndef _NPPATTERN_
 #define _NPPATTERN_
 
-class NPPattern{
+class NPPattern
+{
 protected:
-  Adafruit_NeoPixel*strip;
-	int brightness;
+  Adafruit_NeoPixel *strip;
+  int brightness;
+
 public:
-	NPPattern(Adafruit_NeoPixel*inStrip, int brightness):strip(inStrip),brightness(brightness){}
-	
-	virtual ~NPPattern(){}
-	
-	virtual void run(){};
-	
-	// Input a value 0 to 255 to get a color value.
-	// The colours are a transition r - g - b - back to r.
-	uint32_t wheel(byte wheelPos) {
-		wheelPos = 255 - wheelPos;
-		if(wheelPos < 85) {
-			return strip->Color(255 - wheelPos * 3, 0, wheelPos * 3);
-		}
-		if(wheelPos < 170) {
-			wheelPos -= 85;
-			return strip->Color(0, wheelPos * 3, 255 - wheelPos * 3);
-		}
-		wheelPos -= 170;
-		return strip->Color(wheelPos * 3, 255 - wheelPos * 3, 0);
-	}
-	
-	virtual void setBrightness(int inBrightness){
-		strip->setBrightness(inBrightness);
-		brightness=strip->getBrightness();
-	}
-	
-	virtual int getBrightness(){return brightness;}
+  NPPattern(Adafruit_NeoPixel *inStrip, int brightness)
+      : strip(inStrip)
+      , brightness(brightness)
+  {
+  }
+
+  virtual ~NPPattern()
+  {
+  }
+
+  virtual void run(){};
+
+  // Input a value 0 to 255 to get a color value.
+  // The colours are a transition r - g - b - back to r.
+  uint32_t wheel(byte wheelPos)
+  {
+    wheelPos = 255 - wheelPos;
+    if (wheelPos < 85)
+    {
+      return strip->Color(255 - wheelPos * 3, 0, wheelPos * 3);
+    }
+    if (wheelPos < 170)
+    {
+      wheelPos -= 85;
+      return strip->Color(0, wheelPos * 3, 255 - wheelPos * 3);
+    }
+    wheelPos -= 170;
+    return strip->Color(wheelPos * 3, 255 - wheelPos * 3, 0);
+  }
+
+  virtual void setBrightness(int inBrightness)
+  {
+    strip->setBrightness(inBrightness);
+    brightness = strip->getBrightness();
+  }
+
+  virtual int getBrightness()
+  {
+    return brightness;
+  }
 };
 
 #endif
--- library/examples/AppManager/RainbowChase.h
+++ library/examples/AppManager/RainbowChase.h
@@ -3,32 +3,55 @@
 
 #include "NPPattern.h"
 
-class RainbowChase: public NPPattern{
-	unsigned long wait,next;
-	int j,q;
+class RainbowChase : public NPPattern
+{
+  unsigned long wait, next;
+  int j, q;
+
 public:
-  RainbowChase(Adafruit_NeoPixel*inStrip, uint8_t inWait):NPPattern(inStrip,31),wait(inWait),j(0),q(0){
-		next=millis();
+  RainbowChase(Adafruit_NeoPixel *inStrip, uint8_t inWait)
+      : NPPattern(inStrip, 31)
+      , wait(inWait)
+      , j(0)
+      , q(0)
+  {
+    next = millis();
   }
-	
-  virtual ~RainbowChase(){}
-	
-  void run(){
-		if(next < millis()){
-			next=millis()+wait;
-			if(j<256){
-				if(q<3){
-					for(uint16_t i=0; i< strip->numPixels(); i+=3) {
-						strip->setPixelColor(i+q, 0);
-						strip->setPixelColor((i+q+1)%strip->numPixels(), wheel((i+j) % 255));
-					}
-					strip->setBrightness(brightness);
-					strip->show();
-					q++;
-				}else{q=0;}
-				if(q>=3)j++;
-			}else{j=0;}
-		}
+
+  virtual ~RainbowChase()
+  {
+  }
+
+  void run()
+  {
+    if (next < millis())
+    {
+      next = millis() + wait;
+      if (j < 256)
+      {
+        if (q < 3)
+        {
+          for (uint16_t i = 0; i < strip->numPixels(); i += 3)
+          {
+            strip->setPixelColor(i + q, 0);
+            strip->setPixelColor((i + q + 1) % strip->numPixels(), wheel((i + j) % 255));
+          }
+          strip->setBrightness(brightness);
+          strip->show();
+          q++;
+        }
+        else
+        {
+          q = 0;
+        }
+        if (q >= 3)
+          j++;
+      }
+      else
+      {
+        j = 0;
+      }
+    }
   }
 };
 
